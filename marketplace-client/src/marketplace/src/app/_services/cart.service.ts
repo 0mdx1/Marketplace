a3a6568756df4a732038e8c8db5946e21337efe2
ZadorMax <maxzador01@gmail.com>
2021-06-02 17:04:29 +0300

    cart is working, need to test with products

--- marketplace-client/src/marketplace/src/app/_services/cart.service.ts
+++ marketplace-client/src/marketplace/src/app/_services/cart.service.ts
@@ -45,8 +45,9 @@ export class CartService {
     this.cartTotal$.next(this.cartDataServer.total);
     this.cartDataObs$.next(this.cartDataServer);
 
-    let info: CartModelPublic= null as any;
-    //= JSON.parse(localStorage.getItem('cart'));
+    let info: CartModelPublic=JSON.parse(localStorage.getItem('cart')  || null as any);
+      //null as any;
+      //JSON.parse(localStorage.getItem('cart')  || '{}');
     // not working localStorage
 
     if (info !== null && info !== undefined && info.prodData[0].incart !== 0) {
