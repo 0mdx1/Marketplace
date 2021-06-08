ff2afb4f8136aa8fdd95e9c8cd387f5abc798eee
Kate <kate.lisov@gmail.com>
2021-06-08 12:48:21 +0300

    Added responsive menu.

--- marketplace-client/src/marketplace/src/app/app-routing.module.ts
+++ marketplace-client/src/marketplace/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import {HomeComponent} from './home/home.component';
 import {AuthGuardService} from './_auth/auth.guard.service';
 import {Role} from "./_models/role";
 import {RoleGuardService} from "./_auth/auth.guard.role.service";
+import {MainLayoutComponent} from "./main/layout/main.layout.component";
 
 const accountModule = () => import('./account/account.module').then(x => x.AccountModule);
 
 const routes: Routes = [
   //{ path: 'home', component: HomeComponent, canActivate: [RoleGuardService], data: { roles: [Role.Admin]}},
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuardService]},
+  //{ path: 'home', component: HomeComponent, canActivate: [AuthGuardService]},
+  { path: 'home', component: HomeComponent},
   { path: '', loadChildren: accountModule },
   { path: '**', redirectTo: 'login' }
 ];
