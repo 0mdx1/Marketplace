4e050d516e71282d7b3aadaf25b47d049698a857
Ivan Repeta <ivan.repeta.work@gmail.com>
2021-06-16 14:43:22 +0300

    Add phone number and birthday to default user

--- marketplace-server/src/main/java/com/ncgroup/marketplaceserver/security/config/DefaultUserConfiguration.java
+++ marketplace-server/src/main/java/com/ncgroup/marketplaceserver/security/config/DefaultUserConfiguration.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+
 @Configuration
 public class DefaultUserConfiguration extends UserConfiguration{
 
@@ -17,6 +19,9 @@ public class DefaultUserConfiguration extends UserConfiguration{
 
     @Value("${default-user.surname}")
     private String surname;
+
+    @Value("${default-user.phone}")
+    private String phone;
 
     @Value("${default-user.email}")
     private String email;
@@ -34,6 +39,8 @@ public class DefaultUserConfiguration extends UserConfiguration{
                 .builder()
                 .name(name)
                 .surname(surname)
+                .phone(phone)
+                .birthday(LocalDate.now())
                 .email(email)
                 .password(password)
                 .role(Role.ROLE_USER)
