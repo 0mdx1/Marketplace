83181281f4696dd46a8069c488d84b77afe34f02
0mdx1 <0x0mickendel@gmail.com>
2021-06-18 23:30:08 +0300

    Added http request

--- marketplace-client/src/marketplace/src/app/account/update/update.account.ts
+++ marketplace-client/src/marketplace/src/app/account/update/update.account.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { AbstractControl, FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { User } from "src/app/_models/user";
+import { AccountService } from "src/app/_services/account.service";
 
 @Component({
   templateUrl: './update.account.html'
@@ -7,30 +9,43 @@ import { AbstractControl, FormBuilder, FormGroup, Validators } from "@angular/fo
 export class UpdateAccount implements OnInit {
 
   updateForm: FormGroup;
+  user: User = {};
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private accountService: AccountService) {
     this.updateForm = this.formBuilder.group({
       name: [''],
       surname: [''],
       phone: ['', [Validators.pattern(/\+380[0-9]{9}/)]],
       birthday: ['']
-    })
+    });
   }
 
   get getForm(): { [p: string]: AbstractControl } { return this.updateForm.controls; }
 
   ngOnInit(): void {
-    throw new Error("Method not implemented.");
+    this.accountService.getUser().subscribe(data => {
+      this.user = data
+      this.updateForm.patchValue({
+        name: this.user.name,
+        surname: this.user.surname,
+        phone: this.user.phone,
+        birthday: this.user.dateOfBirth
+      });
+    });
   }
 
   onSubmit() {
-    const obj = {
+    const body = {
       name: this.updateForm.value['name'],
       surname: this.updateForm.value['surname'],
       phone: this.updateForm.value['phone'],
       birthday: this.updateForm.value['birthday']
     }
-    console.log(obj);
+    
+    this.accountService.updateUser(body).subscribe(
+      data => console.log('Success ' + data),
+      error => console.log(error)
+    )
     
     
   }
