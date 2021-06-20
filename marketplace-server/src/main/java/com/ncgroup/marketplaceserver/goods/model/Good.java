0f09676b6b2ce6f069d51ead02823c7d036431be
DANIIL DIMOGLO <52081902+dandimog@users.noreply.github.com>
2021-06-20 22:32:58 +0300

    Product logic fixes (#119)
    * Capacity
    
    * Logic create good modification
    
    * Edit status logic

--- marketplace-server/src/main/java/com/ncgroup/marketplaceserver/goods/model/Good.java
+++ marketplace-server/src/main/java/com/ncgroup/marketplaceserver/goods/model/Good.java
@@ -16,12 +16,8 @@ import java.time.format.DateTimeFormatter;
 @Data
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
 @Slf4j
 public class Good {
-
-//    static final String EUROPEAN_DATE_PATTERN = "dd.MM.yyyy";
-//    static final DateTimeFormatter EUROPEAN_DATE_FORMATTER = DateTimeFormatter.ofPattern(EUROPEAN_DATE_PATTERN);
 
     private long id;
     private String goodName;
@@ -41,15 +37,9 @@ public class Good {
     private String image;
     private boolean status;
 
-//    public void setShippingDate(LocalDate date) {
-//        System.out.println(date);
-//        String dat = String.valueOf(date);
-//        this.shippingDate = LocalDate.parse(dat, EUROPEAN_DATE_FORMATTER);
-//    }
-
-//    public void setPrice(double price, double discount) {
-//        this.price = price - (price * (discount / 100));
-//    }
+    public Good() {
+        this.setStatus(true);
+    }
 
 //    , MediaService mediaService
     public void setProperties(GoodDto goodDto, Long id) {
@@ -59,7 +49,6 @@ public class Good {
         this.setGoodName(goodDto.getGoodName().toLowerCase());
         this.setFirmName(goodDto.getFirmName().toLowerCase());
         this.setQuantity(goodDto.getQuantity());
-//        this.setPrice(goodDto.getPrice(), goodDto.getDiscount());
         this.setPrice(goodDto.getPrice());
         this.setDiscount(goodDto.getDiscount());
         this.setInStock(goodDto.isInStock());
