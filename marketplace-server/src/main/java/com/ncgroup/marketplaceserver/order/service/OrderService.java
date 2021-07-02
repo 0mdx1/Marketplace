58d9ab24a45a3c926550a4affcfce71e5d2a25d9
Yuliia Nortman <ynortman@gmail.com>
2021-07-02 10:37:58 +0300

    Fix order time bug

--- marketplace-server/src/main/java/com/ncgroup/marketplaceserver/order/service/OrderService.java
+++ marketplace-server/src/main/java/com/ncgroup/marketplaceserver/order/service/OrderService.java
@@ -1,17 +1,17 @@
 package com.ncgroup.marketplaceserver.order.service;
 
-import java.time.LocalDateTime;
+
 import java.time.OffsetDateTime;
 import java.util.List;
-import java.util.Map;
 
 import com.ncgroup.marketplaceserver.model.dto.UserDisplayInfoDto;
 import com.ncgroup.marketplaceserver.order.model.OrderStatus;
+import com.ncgroup.marketplaceserver.order.model.dto.OrderPageDto;
 import com.ncgroup.marketplaceserver.order.model.dto.OrderPostDto;
 import com.ncgroup.marketplaceserver.order.model.dto.OrderReadDto;
 
 public interface OrderService {
-	Map<String, Object> getCourierOrders(String token, int page);
+	OrderPageDto getCourierOrders(String token, int page);
 	List<OffsetDateTime> getFreeSlots();
 	OrderReadDto getOrder(long id);
 	OrderReadDto addOrder(OrderPostDto order, String token);
