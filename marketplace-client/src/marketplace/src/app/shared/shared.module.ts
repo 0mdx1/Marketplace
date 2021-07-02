bb663621363cfae591970647c39746ec0f7fc6a3
Yuliia Nortman <ynortman@gmail.com>
2021-07-02 13:41:55 +0300

    Add loader component

--- marketplace-client/src/marketplace/src/app/shared/shared.module.ts
+++ marketplace-client/src/marketplace/src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StockPipe } from './_pipes/stock.pipe';
 import { UnitPipe } from './_pipes/unit.pipe';
+import { LoaderComponent } from './_components/loader/loader.component';
 
 @NgModule({
-  declarations: [StockPipe, UnitPipe],
+  declarations: [StockPipe, UnitPipe, LoaderComponent],
   imports: [CommonModule],
-  exports: [StockPipe, UnitPipe],
+  exports: [StockPipe, UnitPipe, LoaderComponent],
 })
 export class SharedModule {}
